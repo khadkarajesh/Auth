c81971580200040028a5633a080bab7487408292
Rajesh <rajesh.k.khadka@gmail.com>
2018-11-19 11:35:54 +0545

    remove unused endpoints

--- auth/src/main/java/com/smartmobe/auth/rest/service/AuthService.kt
+++ auth/src/main/java/com/smartmobe/auth/rest/service/AuthService.kt
@@ -1,14 +1,12 @@
 package com.smartmobe.kservice.data.rest.service
 
-import com.crushcoder.kmovies.rest.request.ChangePasswordBody
 import com.smartmobe.auth.data.rest.request.ForgotPasswordBody
-import com.smartmobe.auth.data.rest.request.PasswordCreateBody
 import com.smartmobe.kservice.data.rest.request.LoginBody
-import com.smartmobe.kservice.data.rest.response.User
 import kotlinx.coroutines.Deferred
-import okhttp3.MultipartBody
 import okhttp3.ResponseBody
-import retrofit2.http.*
+import retrofit2.http.Body
+import retrofit2.http.POST
+import retrofit2.http.Url
 
 interface AuthService {
     @POST
@@ -17,19 +15,5 @@ interface AuthService {
     @POST
     fun forgotPassword(@Url url: String, @Body body: ForgotPasswordBody): Deferred<Unit>
 
-    @PATCH("password/reset")
-    fun createPassword(@Body passwordBody: PasswordCreateBody): Deferred<Unit>
 
-    @PUT("auth/password")
-    fun updatePassword(@Body changePasswordBody: ChangePasswordBody): Deferred<Unit>
-
-    @GET("auth/me")
-    fun getUser(): Deferred<String>
-
-    @PUT("auth/me")
-    fun updateUser(@Body user: User?): Deferred<User>
-
-    @Multipart
-    @POST("auth/me/avatar")
-    fun updateProfilePicture(@Part image: MultipartBody.Part): Deferred<User>
 }
